52c9a9aabb54162d97f89fcf6b2d92b091800d88
Rafiirsyads <rafiirsyad2004@gmail.com>
2023-09-10 20:52:04 +0700

    menambahkan field

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Book',
-        'amount': '5',
-        'description': 'Encyclopedia'
+        'name': 'Rafi Irsyad Saharso',
+        'npm': '2206082221',
+        'class': 'A'
     }
 
     return render(request, "main.html", context)
